a2f6367ce9cf7dafb5c9169a0adf4cf0518b073b
Travis Dixon <the.trav@gmail.com>
2011-05-20 08:39:38 +1000

    handle multiple lines in the world that aren't necessarily axis aligned.

--- src/main/scala/oae/Physics.scala
+++ src/main/scala/oae/Physics.scala
@@ -3,6 +3,7 @@ package oae
 import physics._
 import java.awt._
 import java.awt.geom._
+import scala.collection.immutable.List
 
 case class Segment(a:Coord, b:Coord, normal:Coord = Coord(0,0)) {
   def intersect(other:Segment) = {
@@ -57,7 +58,11 @@ object Physics {
   var id_counter = 0
   var entities = Map[Int, Entity]()
 
-  var floor = Segment(Coord(100000000,200), Coord(-100000000,200), Coord(0,-1))
+  var world = List(Segment(Coord(105,200), Coord(-105,200), Coord(0,-1)),
+                   Segment(Coord(205,-20), Coord(-205,-20), Coord(0,-1)),
+                   Segment(Coord(200,-25), Coord(100,205), Coord(0,-1)),
+                   Segment(Coord(-100,205), Coord(-200,-25), Coord(0,-1))
+  )
 
 
   def addEntity(pos:Coord, size:Double) = {
@@ -75,58 +80,64 @@ object Physics {
     entities += (id -> entities(id).addAccel(accel))
   }
 
-
-
   def handleCollisions(id:Int, lastFrame:Map[Int, Entity], currentFrame:Map[Int, Entity]) = {
     val entity = currentFrame(id)
     val oldEntity = lastFrame(id)
     val motion = Segment(oldEntity.pos, entity.pos)
     val lossOnImpact = 0.6
 
-    motion.intersect(floor) match {
-      case None => entity
-      case Some(c:Coord) => {
-
-        def perform(segment:Segment, motion:Segment, intersect:Coord) = {
-          //end pos = flip end coord.y
-          val endPos = Coord(motion.b.x, -1 * motion.b.y)
-          //newVel == motion.flip y
-          val motionVector = motion.vector
-          val newVel = motionVector.copy(y=motionVector.y * -1)
-//          println("oldVel,"+motionVector+"\nnewVel,"+newVel)
-          (endPos, newVel)
-        }
-
-        def performWithRotation(angle:Double, segment:Segment, motion:Segment, intersect:Coord) = {
-          val rotatedSegment = segment.rotate(angle)
-          val rotatedMotion = motion.rotate(angle)
-          val rotatedIntersect = intersect.rotate(angle)
-          val translation = Coord(0, -1 * rotatedIntersect.y)
-
-          val tuple = perform(rotatedSegment   + translation,
-                              rotatedMotion    + translation,
-                              rotatedIntersect + translation)
-//          println("tuple,"+tuple)
-          val unTranslated = (tuple._1 - translation, tuple._2)
-//          println("untranslated"+unTranslated)
-          val unRotated = (unTranslated._1.rotate(-1 * angle), unTranslated._2.rotate(-1 * angle))
-//          println("unrotated"+unRotated)
-          unRotated
-        }
-
-        if(motion.vector.dot(floor.normal) < 0) { //ensures motion is towards the segment rather than away (stops double collisions)
-          val angleOfNormal = angleBetween(floor, Segment(Coord(-1,0), Coord(1,0)))
-
-          val tuple = performWithRotation(angleOfNormal, floor, motion, c)
-          val newPos = tuple._1
-          val newVelocity = tuple._2
-
-          entity.copy(pos = newPos, vel = newVelocity)
-        } else {
-          entity
+    val collisions = for (surface <- world) yield {
+      motion.intersect(surface) match {
+        case None => entity
+        case Some(c:Coord) => {
+          def perform(segment:Segment, motion:Segment, intersect:Coord) = {
+            //end pos = flip end coord.y
+            val endPos = Coord(motion.b.x, -1 * motion.b.y)
+            //newVel == motion.flip y
+            val motionVector = motion.vector
+            val newVel = motionVector.copy(y=motionVector.y * -1)
+  //          println("oldVel,"+motionVector+"\nnewVel,"+newVel)
+            (endPos, newVel)
+          }
+  
+          def performWithRotation(angle:Double, segment:Segment, motion:Segment, intersect:Coord) = {
+            val rotatedSegment = segment.rotate(angle)
+            val rotatedMotion = motion.rotate(angle)
+            val rotatedIntersect = intersect.rotate(angle)
+            val translation = Coord(0, -1 * rotatedIntersect.y)
+  
+            val tuple = perform(rotatedSegment   + translation,
+                                rotatedMotion    + translation,
+                                rotatedIntersect + translation)
+  //          println("tuple,"+tuple)
+            val unTranslated = (tuple._1 - translation, tuple._2)
+  //          println("untranslated"+unTranslated)
+            val unRotated = (unTranslated._1.rotate(-1 * angle), unTranslated._2.rotate(-1 * angle))
+  //          println("unrotated"+unRotated)
+            unRotated
+          }
+  
+          if(motion.vector.dot(surface.normal) < 0) { //ensures motion is towards the segment rather than away (stops double collisions)
+            val angleOfNormal = angleBetween(surface, Segment(Coord(-1,0), Coord(1,0)))
+  
+            val tuple = performWithRotation(angleOfNormal, surface, motion, c)
+            val newPos = tuple._1
+            val newVelocity = tuple._2
+  
+            entity.copy(pos = newPos, vel = newVelocity)
+          } else {
+            entity
+          }
         }
       }
     }
+
+    //taking the first collision is a really crap way of solving the Time Of Impact problem
+    collisions.find(_ != entity) match {
+      case None => entity
+      case Some(e) => e
+    }
+
   }
 
   def angleBetween(a:Segment, b:Segment) = {
@@ -152,7 +163,11 @@ object Physics {
   def int(d:Double) = d.asInstanceOf[Int]
   def draw(g:Graphics2D) {
     g.setColor(Color.green)
-    g.drawLine(int(floor.a.x), int(floor.a.y), int(floor.b.x), int(floor.b.y))
+    world.foreach((segment:Segment) => {
+      g.drawLine(int(segment.a.x), int(segment.a.y),
+                 int(segment.b.x), int(segment.b.y))
+    })
+
     entities.foreach((tuple:(Int, Entity)) => {
       val e = tuple._2
       g.draw(new Ellipse2D.Double(e.x, e.y, e.size, e.size))
